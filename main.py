1a00e3f0b4f7dd018598b6645c9240c11d11de8f
cmsong <dlcmaldmlzldxkdlrj@gmail.com>
2022-09-21 20:37:43 +0900

    전투수 개선

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-from cProfile import label
-from datetime import datetime, timedelta
 import json
+from cProfile import label
+from datetime import datetime
+from typing import Callable
 
-from define import ResponseData, response_data_from_dict
+from define import Item, response_data_from_dict
 
 print("asdf")
 
+is_not_new: Callable[[Item], bool] = lambda i: i.days_in_clan > 30
 
-f = open("./data.json", "r", encoding="UTF-8")
 
-resp= response_data_from_dict(json.load(f))
+with open("./data.json", "r", encoding="UTF-8") as f:
+    resp = response_data_from_dict(json.load(f))
 
-# 최근 접속일 기준
-items = sorted(resp.items, key=lambda item: item.last_battle_time)
+    # 최근 접속일 기준
+    # items = sorted(resp.items, key=lambda item: item.last_battle_time)
 
-# 하루 전투수 기준
-# items = sorted(resp.items, key=lambda item: item.battles_per_day)
-# items.reverse()
+    # 하루 전투수 기준
+    items = sorted(resp.items, key=lambda item: item.battles_per_day)
+    itmes = list(filter(is_not_new, items))
+    items = items[-10:]
 
-for item in items:
-    print(f"{item.name.rjust(30)}\t{datetime.fromtimestamp(item.last_battle_time).strftime('%Y-%m-%d %H:%M:%S')}\t{item.battles_per_day}")
+    for item in items:
+        print(f"{item.name.rjust(30)}\t{datetime.fromtimestamp(item.last_battle_time).strftime('%Y-%m-%d %H:%M:%S')}\t{item.battles_per_day}")
 
-# for i in range(0, 10):
-#     item = items[i]
-#     print(f"{item.name}\t{datetime.fromtimestamp(item.last_battle_time).strftime('%Y-%m-%d %H:%M:%S')}")
-
+    # for i in range(0, 10):
+    #     item = items[i]
+    #     print(f"{item.name}\t{datetime.fromtimestamp(item.last_battle_time).strftime('%Y-%m-%d %H:%M:%S')}")
